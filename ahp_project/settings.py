9ffac92554dd96ded81e430c40a41135f344566a
zjkz <qjkzzz@gmail.com>
2015-05-05 01:15:50 +0300

    new email settings

--- ahp_project/settings.py
+++ ahp_project/settings.py
@@ -78,9 +78,10 @@ STATIC_URL = '/static/'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
 EMAIL_HOST_USER = 'analytichierarchysystem@gmail.com'
 EMAIL_HOST_PASSWORD = 'ghbphfrb'
+EMAIL_USE_TLS = True
+
+
 
 LOGIN_URL = '/ahp/login/'
