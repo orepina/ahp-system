f625209dea1c06f13fc873f06369097f9ea98c7e
zjkz <qjkzzz@gmail.com>
2015-05-05 00:00:32 +0300

    project urls

--- ahp_project/urls.py
+++ ahp_project/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from ahp import views
 
 urlpatterns = patterns('',
     url(r'^ahp/', include('ahp.urls')),
-    url(r'^$/', include('ahp.urls')),
+    url('', include('ahp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
